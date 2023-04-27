6d3847f04114d79e89bfe5fc00e15ade1654a311
Neomi Lee <neomilee1@gmail.com>
2023-04-26 19:42:20 -0700

    commit tree 178a7877da771792085d2b135b66a532408c1435

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate"){
+    let message = `This Saturday, I want to ${activity}!`;
+    return message;
+};
+function mondayWork (where = "go to the office"){
+    let message = `This Monday, I will ${where}.`;
+    return message;
+};
+
+
+function wrapAdjective(symbol = "*") {
+    const innerFunction = function (adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+    return innerFunction;
+}
